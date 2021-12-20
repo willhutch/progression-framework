e0e020e099bbd56fca037d27331715fcec48bf86
willhutch <will-hutch@hotmail.com>
2021-12-20 21:48:05 +0000

    add basic Global Branding

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 // @flow
 import * as React from 'react'
 import { graphql } from 'gatsby'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub } from '@fortawesome/free-brands-svg-icons'
+//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+//import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import Layout from '../components/layout'
 import {
   Subtitle,
@@ -25,20 +25,18 @@ export default class Index extends React.Component<Props> {
         <DescriptionView>
           <DescriptionTitleGroup>
             <Subtitle>Welcome To</Subtitle>
-            <Title>Progression at Monzo 🎉</Title>
+            <Title>Progression at Global 🎉</Title>
           </DescriptionTitleGroup>
           <Description>
             <DescriptionText>
-              This is where we keep the progression frameworks we use at Monzo.
+              This is where we keep the progression frameworks we use at Global.
+              This framework is based on the Monzo career framework which we have
+              copied and made to fit Global.
             </DescriptionText>
             <DescriptionText>
               Progression frameworks are tools that help us evaluate and pay our
               staff consistently, and help people understand how to progress in
               their work.
-            </DescriptionText>
-            <DescriptionText>
-              We&apos;ve put them here for everyone at Monzo (and anyone in the
-              world!) to use.
             </DescriptionText>
             <DescriptionText>
               We&apos;ll be adding more frameworks as we build them, and
@@ -50,20 +48,6 @@ export default class Index extends React.Component<Props> {
               source. That means you can contribute to the development of this
               site or our progression frameworks, and use both as inspiration
               for your own projects!
-            </DescriptionText>
-            <DescriptionText>
-              To get involved, go to{' '}
-              <a href="https://github.com/monzo/progression-framework">
-                <FontAwesomeIcon icon={faGithub} />{' '}
-                @monzo/progression-framework.
-              </a>
-            </DescriptionText>
-            <DescriptionText>
-              You can find out more about progression at Monzo and what it means
-              for us in this{' '}
-              <a href="https://monzo.com/blog/2019/01/07/progression">
-                blog post.
-              </a>
             </DescriptionText>
             <DescriptionText>
               Take a look around and let us know what you think! 🚀
